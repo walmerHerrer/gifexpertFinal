5bb050b7fc50de6a1fd22a781456f1660d820933
walmerHerrer <0512018020@alumnos.unp.edu.pe>
2023-11-08 12:06:32 -0500

    cambio de componenete

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CounterWithCustomHook />
+    <GifExpertApp />
   </React.StrictMode>
 );
